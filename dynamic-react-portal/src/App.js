6a60a9b098d4c530e2fd91d31bc4ad13296ee82a
gxwadha <gaurav.wadhawan@usbank.com>
2021-09-22 11:39:42 -0700

    First

---
+++ dynamic-react-portal/src/App.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import List from './components/list';
+import withListLoading from './components/withListLoading';
+function App() {
+  const ListLoading = withListLoading(List);
+  const [appState, setAppState] = useState({
+    loading: false,
+    repos: null,
+  });
+
+  useEffect(() => {
+    setAppState({ loading: true });
+    const apiUrl = `https://api.github.com/users/gauravwd22/repos`;
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((repos) => {
+        setAppState({ loading: false, repos: repos });
+      });
+  }, [setAppState]);
+  return (
+    <div className='App'>
+      <div className='container'>
+        <h1>My Repositories</h1>
+      </div>
+      <div className='repo-container'>
+        <ListLoading isLoading={appState.loading} repos={appState.repos} />
+      </div>
+      <footer>
+        <div className='footer'>
+          Built{' '}
+          by Gaurav Wadhawan
+        </div>
+      </footer>
+    </div>
+  );
+}
+export default App;
